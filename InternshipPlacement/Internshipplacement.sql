ba04c55f3f4786a9222ef483f510ae276d7ab539
Absolomjr <145746728+Absolomjr@users.noreply.github.com>
2025-03-03 23:21:10 +0300

    Update Internshipplacement.sql

--- InternshipPlacement/Internshipplacement.sql
+++ InternshipPlacement/Internshipplacement.sql
@@ -1,1 +1,10 @@
-
+This is an InternShip Placement System.
+  -- PROBLEM STATEMENT
+     Many students struggle to secure internships and full-time jobopportunities due to difficulties in tracking applications,
+  managing employer connections, and receiving timelyfeedback. The lack of a centralized platform leads to misseddeadlines,
+  inadequate guidance, and ineffective follow-ups.On the other hand, employers face challenges in 
+  managing ahigh volume of applications, efficiently evaluating candidates,and streamlining the hiring process. 
+  The absence of astructured system results in delays, overlooked talent, andinefficiencies in recruitment.
+  This project aims to bridge the gap between students andemployers by developing a smart,
+  user-friendly internship andjob placement system that simplifies application tracking,enhances employer-student interactions,
+  and improves thehiring experience for both parties
